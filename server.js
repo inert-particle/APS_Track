232f9ff3e6ecb74bc1f27200f239a19f61a8e00f
inert-particle <sumit.kumar1u7@gmail.com>
2026-02-21 19:11:33 +0530

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const ShipmentPieceDetails = require('./ShipmentPieceDetails');
 const KYCDetails = require('./KYCDetails');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve frontend files
 app.use(express.static("public"));
